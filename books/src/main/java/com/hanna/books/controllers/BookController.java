c8f8173f31d146473f82f9119eca702b26a620ea
HP <hmpettersson@gmx.com>
2021-01-20 16:45:24 +0100

    changes in bookcontroller

--- books/src/main/java/com/hanna/books/controllers/BookController.java
+++ books/src/main/java/com/hanna/books/controllers/BookController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanna.books.models.Book;
 import com.hanna.books.repositories.BookRepository;
 
-
 @RestController
+@RequestMapping("/rest")
 public class BookController {
 
     @Autowired
